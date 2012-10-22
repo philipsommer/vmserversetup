a99a6feb1623e572e0ce060ac48ab7679b34d182
Philip Sommer <psommer@vz.net>
2012-10-22 16:05:08 +0200

    first Vagrant setup

---
+++ manifests/nature-server-config.pp
@@ -0,0 +1,40 @@
+#defines default values for subsequent calls of exec
+Exec {
+   path => ["/bin", "/sbin", "/usr/bin", "/usr/sbin", "/usr/local/bin"],
+   user => $user,
+   cwd => $home,
+   logoutput => "on_failure",
+}
+
+#defines default values for subsequent calls of file
+File {
+   owner => $user,
+   group => $user,
+   mode => 0644,
+}
+
+#defines default values for subsequent calls of package
+Package {
+   ensure => latest,
+}
+
+exec {"apt-get_update":
+       command => "apt-get update",
+       before => Package["base_cmd_tools"],
+       user => root,
+   }
+
+package {"base_cmd_tools":
+       name => [
+           "build-essential", #essentials :-)
+           "curl", #call url
+           "git", # vcs
+           "htop", #human readable top (processes)
+           "landscape-common",
+           "default-jre",
+           "jetty",
+           "mongodb",
+           "nano",
+           "maven2",
+       ],
+   }
